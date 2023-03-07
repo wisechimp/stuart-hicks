fc7a5cfab47507d333af0fd5934bd7940b94facd
shicks1080 <stuart.hicks@gmail.com>
2023-03-07 17:31:50 +0100

    fix: Correcting some migration details

--- stuart-hicks-frontend/gatsby-config.ts
+++ stuart-hicks-frontend/gatsby-config.ts
@@ -1,38 +1,47 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `stuart-hicks-frontend`,
-    siteUrl: `https://www.stuarthicks.devß`
+    siteUrl: `https://www.stuarthicks.dev`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [{
-    resolve: 'gatsby-source-sanity',
-    options: {
-      "projectId": "rm6ee25a",
-      "dataset": "production"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: "gatsby-plugin-google-gtag",
-    options: {
-      trackingId: "G-VK0C88C1KS",
-    }
-  }, "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/sh-logo.png"
-    }
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: "rm6ee25a",
+        dataset: "production",
+      },
     },
-    __key: "images"
-  }]
-};
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-plugin-google-gtag",
+      options: {
+        trackingId: "G-VK0C88C1KS",
+      },
+    },
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/sh-logo.png",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+}
 
-export default config;
+export default config
--- stuart-hicks-frontend/src/pages/index.tsx
+++ stuart-hicks-frontend/src/pages/index.tsx
@@ -1,12 +1,10 @@
 import * as React from "react"
 
-import Layout from 'components/layout/Layout'
+import Layout from "../components/layout/Layout"
 
 // markup
 const Home = () => {
-  return (
-    <Layout pageTitle="Stuart Hicks - React Frontend Developer"></Layout>
-  )
+  return <Layout pageTitle='Stuart Hicks - React Frontend Developer'></Layout>
 }
 
 export default Home
