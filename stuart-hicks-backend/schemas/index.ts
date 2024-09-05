0bb75a6b7e125f688b7b9229d9b5257abddd1f98
shicks1080 <stuart.hicks@gmail.com>
2024-09-05 15:05:33 +0200

    ci: Cleaning out previous project files
    They've preventing the build for deployment

--- stuart-hicks-backend/schemas/index.ts
+++
@@ -1,7 +0,0 @@
-import blockContent from './blockContent'
-import category from './tech'
-import project from './project'
-import work from './work'
-import techIconObject from './customSchemas/techIconObject'
-
-export const schemaTypes = [project, work, category, blockContent, techIconObject]
--- stuart-hicks-frontend/src/pages/projects.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Layout from '../components/layout/Layout'
-
-const Projects = () => {
-  return <Layout pageTitle="Personal Projects" />
-}
-
-export default Projects
